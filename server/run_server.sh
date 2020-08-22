8652ff30c6ebf429875a4877a21d68edbe0c8847
spokv <assaf1111@yahoo.com>
2020-08-22 16:47:33 +0300

    remove junk

--- server/run_server.sh
+++
@@ -1,4 +0,0 @@
-#redis-server --protected-mode no --save "" --appendonly no &
-#service apache2 start
-#python run_model_server.py &
-python3 run_ws_server.py --host 192.168.1.192 --port 5000 --stabilize 0
